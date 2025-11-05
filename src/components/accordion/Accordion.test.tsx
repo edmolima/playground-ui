20ce99dc39ed0596c441355c3425613db160ca09
Edmo Lima <soedmolima@gmail.com>
2025-11-05 11:20:09 +0100

    fix some errors in config

--- src/components/accordion/Accordion.test.tsx
+++ src/components/accordion/Accordion.test.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import { render } from "@testing-library/react";
 import { Accordion } from "./Accordion";
 
 describe("Accordion", () => {
   test("should render successfully", () => {
-    const { baseElement } = render(<Accordion title="Test Accordion">Content</Accordion>);
+    const { baseElement } = render(<Accordion title="Test Accordion" content="Content" />);
     expect(baseElement).toBeTruthy();
   });
 });
